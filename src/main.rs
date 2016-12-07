2e76c75a54702297d66a2344f77d702f2ea3614e
C J Silverio <ceejceej@gmail.com>
2016-12-07 08:27:36 -0800

    breaking: Drastically simplify the API.
    emit() --> emit_point()
    emit_value() -> emit()
    
    All type-specific emit functions are gone, in favor of the generic
    function. The emit_name_val_tag() convenience remains, but it takes
    any serde_json serializable type now.
    
    Updated sample usage in main.rs. Updated README. Bumped minor version
    number.

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,15 @@ fn main()
     custom.connect("tcp://localhost:4677");
 
     custom.emit_name("start");
-    custom.emit_value("floating", 232.5);
-    custom.emit_value("also-not-floating", "foo");
-    custom.emit_i32("integer", 2048);
+    custom.emit("floating", 232.5);
+    custom.emit("also-not-floating", "foo");
+    custom.emit("integer", 2048);
 
     let mut point: Point = Point::new();
     point.insert("name", serde_json::to_value("inconvenience"));
     point.insert("tag", serde_json::to_value("subjective"));
     point.insert("value", serde_json::to_value(500.3));
-    custom.emit(point);
+    custom.emit_point(point);
 
     // Create a local emitter with default fields & use it.
     let mut opts: Point = Point::new();
@@ -29,7 +29,7 @@ fn main()
 
     let mut with_defaults = numbat::Emitter::new(opts, "numbat-emitter");
     with_defaults.connect("tcp://localhost:4677");
-    with_defaults.emit_u16("unsigned16", 256_u16);
+    with_defaults.emit("unsigned16", 256_u16);
 
     // Now initialize & use the global emitter.
     let mut opts2: Point = Point::new();
@@ -38,5 +38,5 @@ fn main()
     emitter().init(opts2, "numbat-emitter");
     emitter().connect("tcp://localhost:4677");
     emitter().emit_name("initialization");
-    emitter().emit_name_int_tag_uint("response", 23, "status", 200);
+    emitter().emit_name_val_tag("response", 23, "status", 200);
 }
